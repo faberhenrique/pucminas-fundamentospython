d20c6c8b6094999999b0b93f6b8894c45b1d0310
Faber Xavier <faber.henrique@gmail.com>
2025-04-28 19:53:55 -0300

    classe apenas com assinaturas

---
+++ aula2.py
@@ -0,0 +1,88 @@
+import requests
+import os
+import json
+
+
+WEATHER_KEY = os.getenv("WEATHER_KEY")
+AIRVISUAL_KEY = os.getenv("AIRVISUAL_KEY")
+
+def buscar_clima(cidade):
+    """
+    Consulta o clima atual de uma cidade utilizando a WeatherAPI.
+
+    Args:
+        cidade (str): O nome da cidade para a qual o clima será consultado.
+
+    Returns:
+        dict: Um dicionário contendo os dados do clima retornados pela API, 
+              caso a consulta seja bem-sucedida.
+        None: Retorna None se ocorrer um erro durante a consulta.
+
+    Raises:
+        requests.exceptions.RequestException: Caso ocorra um erro na requisição HTTP.
+    """
+    """Consulta o clima atual de uma cidade na WeatherAPI."""
+    
+
+def buscar_qualidade_ar(cidade, estado, pais):
+    """
+    Consulta a qualidade do ar para uma cidade específica usando a AirVisual API.
+
+    Args:
+        cidade (str): O nome da cidade para a qual deseja consultar a qualidade do ar.
+        estado (str): O estado ou província onde a cidade está localizada.
+        pais (str): O país onde a cidade está localizada.
+
+    Returns:
+        dict: Um dicionário contendo os dados da qualidade do ar retornados pela API, 
+              caso a consulta seja bem-sucedida.
+        None: Retorna None se ocorrer um erro durante a consulta.
+
+    Raises:
+        requests.exceptions.RequestException: Caso ocorra um erro na requisição HTTP.
+    """
+    """Consulta a qualidade do ar usando a AirVisual API."""
+  
+
+def buscar_dados_pais(pais):
+    """
+    Consulta informações sobre um país utilizando a REST Countries API.
+
+    resposta_country = requests.get(url_country)
+    resposta_country.raise_for_status()
+    return resposta_country.json()
+
+    Returns:
+        dict: Um dicionário contendo os dados do país retornados pela API, 
+              caso a consulta seja bem-sucedida.
+        None: Retorna None se ocorrer um erro durante a consulta.
+
+    Raises:
+        requests.exceptions.RequestException: Caso ocorra um erro na requisição HTTP.
+    """
+   
+
+
+# Testando as funções
+def testeFunctions(buscar_clima=buscar_clima, buscar_qualidade_ar=buscar_qualidade_ar, buscar_dados_pais=buscar_dados_pais):
+    clima = buscar_clima("Belo Horizonte")
+    if clima:
+        print("Clima em Belo Horizonte:")
+        print(json.dumps(clima, indent=4, ensure_ascii=False))  
+    arQualidade = buscar_qualidade_ar("Sao Paulo", "Sao Paulo", "Brazil")
+    if arQualidade:
+        print("Qualidade do ar em São Paulo:")
+        print(json.dumps(arQualidade, indent=4, ensure_ascii=False))
+    dadosPais = buscar_dados_pais("Brazil")
+    if dadosPais:
+        print("Dados do país Brasil:")
+        print(json.dumps(dadosPais, indent=4, ensure_ascii=False))
+
+
+def main():
+    testeFunctions()
+
+
+if __name__ == "__main__":
+    main()
+        
